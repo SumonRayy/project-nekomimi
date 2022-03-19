d2d0e917104e22cdc64592e26035c7af03905c75
SumonRayy <sumonrayy@gmail.com>
2022-03-19 23:17:06 +0530

    🚢 new routes Linked to the NavIcons (#16)

--- src/components/nav/NavIcons.js
+++ src/components/nav/NavIcons.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import { userData } from '../../data/userData'
 
 function NavIcons() {
 
     const [user, setUser] = React.useState(false)
+    const [account, setAccount] = React.useState({})
     const [userMenu, setUserMenu] = React.useState(false)
+
+    const history = useHistory()
 
     const handleConnect = () => {
         setUser(true)
@@ -16,13 +19,29 @@ function NavIcons() {
         return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
+    const randomUser = () => {
+        const user = userData[randomIntGen(0, userData.length - 1)]
+        setAccount(user)
+    }
+
     const handleAccount = () => {
         setUserMenu(!userMenu)
+        randomUser()
     }
 
     const handleLogout = () => {
         setUser(false)
         setUserMenu(false)
+    }
+
+    const handleRoutes = (route, id) => {
+        if (route === 'profile') {
+            history.push('/profile' + (id ? '/' + id : ''))
+        } else if (route === 'dashboard') {
+            history.push('/dashboard' + (id ? '/' + id : ''))
+        } else {
+            history.push('/')
+        }
     }
 
     return (
@@ -37,13 +56,13 @@ function NavIcons() {
                             userMenu ? (
                                 <div className='user-menu'>
                                     <div className='user-menu-header'>
-                                        {userData[randomIntGen(0, userData.length - 1)].address.slice(0, 10) + '...'}
-                                        <a>
+                                        {account.address.slice(0, 10) + '...'}
+                                        <a onClick={() => handleRoutes('profile', account.address)}>
                                             Manage Account
                                         </a>
                                     </div>
                                     <div className='user-menu-body'>
-                                        <span>
+                                        <span onClick={() => handleRoutes('dashboard', account.address)}>
                                             <i className="fas fa-user"></i>
                                             Your Channel
                                         </span>
