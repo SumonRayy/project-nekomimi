d54bdf3da6ba413e355ce455eb64c327a540536b
SumonRayy <sumonrayy@gmail.com>
2022-03-19 11:36:16 +0530

    💭 comment data added

---
+++ src/data/commentData.js
@@ -0,0 +1,23 @@
+export const commentData = [
+    {
+        id: 1,
+        comment: 'This is a comment',
+        user: 'John Doe',
+        userImage: 'https://randomuser.me/api/portraits/thumb/' + 'men/' + '10.jpg',
+        time: '20th March'
+    },
+    {
+        id: 2,
+        comment: 'This is a second comment',
+        user: 'Sally Smith',
+        userImage: 'https://randomuser.me/api/portraits/thumb/' + 'women/' + '11.jpg',
+        time: '14th March'
+    },
+    {
+        id: 3,
+        comment: 'This is a third comment',
+        user: 'Mike Cena',
+        userImage: 'https://randomuser.me/api/portraits/' + 'thumb/' + 'men/' + '22.jpg',
+        time: '11th March'
+    },
+];
