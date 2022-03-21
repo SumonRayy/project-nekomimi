c69e4677c4fa9fd882f327b3b5940b8c0b237862
SumonRayy <sumonrayy@gmail.com>
2022-03-21 17:16:10 +0530

    🔃 loading animation added for video uploading

---
+++ src/components/anim/loading.animation.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './Animations.css'
+
+function LoadingAnimation() {
+    return (
+        <div className="loadingio-spinner-double-ring-9piz39ek53d">
+            <div className="ldio-8pccwnec8z7">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingAnimation
